ece546b30327aba673ce29f763d0a2f34610e6af
Zubair Mukhi <zubair.mukhi@gmail.com>
2021-01-12 22:02:35 +0000

    linting complete

--- xp_db.py
+++ xp_db.py
@@ -1,21 +1,41 @@
+'''
+This module contains the ORM stuff for the xp/modifiers for the bot
+'''
 import sqlalchemy
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
 
 engine = sqlalchemy.create_engine('sqlite:///xp.db')
-
 Base = declarative_base()
-
-class discordUser(Base):
-    __tablename__ = 'discord_users'
-    id = Column(Integer, primary_key=True)
-    xp = Column(Integer)
-
-    def __repr__(self):
-        return f'User {self.id}, {self.xp} XP'
-
 Session = sessionmaker(bind=engine)
 
+class DiscordUser(Base):
+    '''
+    This class contains Discord users and tracks their XP.
+    '''
+    __tablename__ = 'discord_users'
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    xp = sqlalchemy.Column(sqlalchemy.Integer)
+    def __repr__(self):
+        '''
+        This is the string rep of a user
+        '''
+        return f'User {self.id}, {self.xp} XP'
+
+    def provide_modifier(self):
+        '''
+        this is a stub
+        '''
+        return self.xp / 3
+
 def make_tables():
+    '''
+    This method will make all tables in the db.
+    '''
     Base.metadata.create_all(engine)
+
+def test():
+    '''
+    linter suck
+    '''
+    return 'linters suck'
