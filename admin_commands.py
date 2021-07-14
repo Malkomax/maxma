eb690880b5eece66d203d2b62fd142a70c20c815
Zubair Mukhi <zubair.mukhi@gmail.com>
2021-07-13 20:19:29 -0400

    randint

--- admin_commands.py
+++ admin_commands.py
@@ -5,6 +5,6 @@ import asyncio
 async def bonk(argument: str) -> str:
     '''bonk'''
     arg = str.strip(argument[len('bonk'):])
-    delay = random(300, 900)
+    delay = random.randint(300, 900)
     await asyncio.sleep(delay)
     return f'*bonks {arg}*'
